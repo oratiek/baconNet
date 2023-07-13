0409239c2620a922be43720c7ff7299b5fedb7ed
hota1024 <hotalog@hota1024.com>
2023-07-13 19:09:45 +0900

    add optin for material3

--- app/src/main/java/io/baconnet/MainActivity.kt
+++ app/src/main/java/io/baconnet/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
@@ -11,6 +12,7 @@ import io.baconnet.ui.pages.FirstTime
 import io.baconnet.ui.theme.Bacon_netTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
