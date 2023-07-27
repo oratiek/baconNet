bd8a30463a47d197852ed13324d1d8ad0345518c
hota1024 <hotalog@hota1024.com>
2023-07-27 23:30:14 +0900

    add like button

--- app/src/main/java/io/baconnet/ui/components/PostCard.kt
+++ app/src/main/java/io/baconnet/ui/components/PostCard.kt
@@ -13,11 +13,20 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.outlined.Favorite
+import androidx.compose.material.icons.outlined.FavoriteBorder
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CardElevation
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
@@ -37,15 +46,23 @@ import kotlinx.datetime.Instant
 import kotlinx.datetime.toJavaInstant
 import java.security.MessageDigest
 import java.util.Date
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import io.baconnet.ui.theme.BaconPink
 
 @Composable
 fun PostCard(
     displayName: String = "<no_name>",
     body: String = "<no_body>",
     isVerified: Boolean = false,
+    isLiked: Boolean = false,
+    likes: Int = 0,
     emailHash: String = "31c5543c1734d25c7206f5fd591525d0295bec6fe84ff82f946a34fe970a1e66",
-    postedAt: Instant = Clock.System.now()
+    postedAt: Instant = Clock.System.now(),
+    onLikeStateChanged: (isLiked: Boolean) -> Unit = {}
 ) {
+    var isLiked by remember { mutableStateOf(isLiked) }
+
     Card(
         modifier = Modifier
             .fillMaxWidth()
@@ -60,7 +77,9 @@ fun PostCard(
                 AsyncImage(
                     model = "https://s.gravatar.com/avatar/$emailHash",
                     contentDescription = "Translated description of what the image contains",
-                    modifier = Modifier.size(48.dp).clip(CircleShape)
+                    modifier = Modifier
+                        .size(48.dp)
+                        .clip(CircleShape)
                 )
             }
             Column(verticalArrangement = Arrangement.spacedBy(8.dp)) {
@@ -90,6 +109,21 @@ fun PostCard(
                         fontWeight = FontWeight.Normal
                     )
                 )
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.End
+                ) {
+                    if (isLiked) {
+                        IconButton(onClick = { isLiked = false; onLikeStateChanged(false); }, modifier = Modifier.size(24.dp)) {
+                            Icon(Icons.Default.Favorite, contentDescription = "unlike", tint = BaconPink)
+                        }
+                    } else {
+                        IconButton(onClick = { isLiked = true; onLikeStateChanged(true); }, modifier = Modifier.size(24.dp)) {
+                            Icon(Icons.Outlined.FavoriteBorder, contentDescription = "like")
+                        }
+                    }
+                    Text(text = " ${likes}")
+                }
             }
         }
     }
